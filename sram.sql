e02a850d182815ac91a63c1bee9d9457f097f220
YannisB81 <blondron.yannis@gmail.com>
2021-05-11 13:38:09 +0200

    sql

---
+++ sram.sql
@@ -0,0 +1,71 @@
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ sram /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+USE sram;
+
+DROP TABLE IF EXISTS doctrine_migration_versions;
+CREATE TABLE `doctrine_migration_versions` (
+  `version` varchar(191) COLLATE utf8_unicode_ci NOT NULL,
+  `executed_at` datetime DEFAULT NULL,
+  `execution_time` int DEFAULT NULL,
+  PRIMARY KEY (`version`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+DROP TABLE IF EXISTS friends;
+CREATE TABLE `friends` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id_id` int NOT NULL,
+  `friend_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_21EE70699D86650F` (`user_id_id`),
+  CONSTRAINT `FK_21EE70699D86650F` FOREIGN KEY (`user_id_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+DROP TABLE IF EXISTS sortie;
+CREATE TABLE `sortie` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `place` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `date` datetime NOT NULL,
+  `description` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `participant_max` int NOT NULL,
+  `organisator` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+DROP TABLE IF EXISTS user;
+CREATE TABLE `user` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `adress` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `phone` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+DROP TABLE IF EXISTS user_has_sortie;
+CREATE TABLE `user_has_sortie` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id_id` int NOT NULL,
+  `sortie_id_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_B77A02859D86650F` (`user_id_id`),
+  KEY `IDX_B77A0285E64A3B53` (`sortie_id_id`),
+  CONSTRAINT `FK_B77A02859D86650F` FOREIGN KEY (`user_id_id`) REFERENCES `user` (`id`),
+  CONSTRAINT `FK_B77A0285E64A3B53` FOREIGN KEY (`sortie_id_id`) REFERENCES `sortie` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+DROP TABLE IF EXISTS wishlist_sortie;
+CREATE TABLE `wishlist_sortie` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id_id` int NOT NULL,
+  `sortie_id_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_8F0D0CB49D86650F` (`user_id_id`),
+  KEY `IDX_8F0D0CB4E64A3B53` (`sortie_id_id`),
+  CONSTRAINT `FK_8F0D0CB49D86650F` FOREIGN KEY (`user_id_id`) REFERENCES `user` (`id`),
+  CONSTRAINT `FK_8F0D0CB4E64A3B53` FOREIGN KEY (`sortie_id_id`) REFERENCES `sortie` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
